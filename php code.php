3beffbd200902668f0005c12051eb0cfc17b46cd
Devendra Sirvi <190304105061@paruluniversity.ac.in>
2020-11-05 21:44:33 +0530

    Added signup page

---
+++ php code.php
@@ -0,0 +1,8 @@
+<?php
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "Devil@007";
+$dbName = "polling";
+
+$conn = mysql_connect($dbServername, $dbUsername, $dbPassword, $dbPassword);
+?>
